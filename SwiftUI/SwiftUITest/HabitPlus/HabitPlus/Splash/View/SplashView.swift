2d39180f95d0756f598b565fb46a22139c3edc28
Mateus Ferreira <Ferreira-Mateus>
2024-06-24 13:31:14 -0300

    2 - Extensions em Views

--- SwiftUI/SwiftUITest/HabitPlus/HabitPlus/Splash/View/SplashView.swift
+++ SwiftUI/SwiftUITest/HabitPlus/HabitPlus/Splash/View/SplashView.swift
@@ -8,7 +8,9 @@ struct SplashView: View {
     var body: some View {
         switch state {
         case .loading:
-            Text("Loading")
+//            LoadingView()
+            //loading
+            loadingView()
         case .goToSigningScreen:
             Text("Signing")
             // TODO: Navegar proxima tela
@@ -17,6 +19,53 @@ struct SplashView: View {
             // TODO: Navegar proxima tela
         case .error(let msg):
             Text("Erro \(msg)")
+        }
+    }
+}
+
+// FORMAS DE UTILIZAR UMA VIEW
+// 1. Compartilhamento | Objetos
+// Reutilizar Objetos e Views
+//struct LoadingView: View {
+//    var body: some View {
+//        ZStack {
+//            Image("logo")
+//                .resizable()
+//                .scaledToFit()
+//                .frame(maxWidth: .infinity, maxHeight: .infinity)
+//                .padding(20)
+//                .background(Color.white)
+//                .ignoresSafeArea()
+//        }
+//    }
+//}
+
+// 2. Variáveis em extensions
+//extension SplashView {
+//    var loading: some View {
+//        ZStack {
+//            Image("logo")
+//                .resizable()
+//                .scaledToFit()
+//                .frame(maxWidth: .infinity, maxHeight: .infinity)
+//                .padding(20)
+//                .background(Color.white)
+//                .ignoresSafeArea()
+//        }
+//    }
+//}
+
+// 3. Funções em extensions
+extension SplashView {
+    func loadingView() -> some View {
+        ZStack {
+            Image("logo")
+                .resizable()
+                .scaledToFit()
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                .padding(20)
+                .background(Color.white)
+                .ignoresSafeArea()
         }
     }
 }
