7b4a3f16bf1260cd0373eebcd3c286ae48e0647d
Mateus Ferreira <Ferreira-Mateus>
2024-06-26 13:29:07 -0300

    9 - Ajustando tela de cadastro

--- SwiftUI/SwiftUITest/HabitPlus/HabitPlus/SignUp/View/SignUpView.swift
+++ SwiftUI/SwiftUITest/HabitPlus/HabitPlus/SignUp/View/SignUpView.swift
@@ -11,7 +11,85 @@ struct SignUpView: View {
     @State var birthday = ""
     
     var body: some View {
-        Text("cadastro")
+        
+        ZStack {
+            
+            ScrollView(showsIndicators: false) {
+                
+                VStack(alignment: .center) {
+                    
+                    VStack(alignment: .leading, spacing: 8) {
+                        
+                        Text("Cadastro")
+                            .foregroundColor(.black)
+                            .font(Font.system(.title).bold())
+                            .padding(.bottom, 8)
+                        
+                        fullNameField
+                        emailField
+                        passwordField
+                        documentField
+                        phoneField
+                        birthdayField
+                        saveButton
+                    }
+                    
+                    Spacer()
+                }.padding(.horizontal, 8)
+                
+            }.padding(20)
+        }
+    }
+}
+
+extension SignUpView {
+    var fullNameField: some View {
+        TextField("", text: $fullName)
+            .border(Color.black)
+    }
+}
+
+extension SignUpView {
+    var emailField: some View {
+        TextField("", text: $email)
+            .border(Color.black)
+    }
+}
+
+extension SignUpView {
+    var passwordField: some View {
+        SecureField("", text: $password)
+            .border(Color.black)
+    }
+}
+
+extension SignUpView {
+    var documentField: some View {
+        TextField("", text: $document)
+            .border(Color.black)
+    }
+}
+
+extension SignUpView {
+    var phoneField: some View {
+        TextField("", text: $phone)
+            .border(Color.black)
+    }
+}
+
+extension SignUpView {
+    var birthdayField: some View {
+        TextField("", text: $birthday)
+            .border(Color.black)
+    }
+}
+
+extension SignUpView {
+    var saveButton: some View {
+        Button("Realize o seu cadastro") {
+            // TODO: Action botão
+        }
+            .padding(.top, 24)
     }
 }
 
