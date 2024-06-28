8e0d5be050a5b754dfd93af4070b59b942452700
Mateus Ferreira <Ferreira-Mateus>
2024-06-28 14:43:44 -0300

    17 - WebService genérico e callbacks

--- SwiftUI/SwiftUITest/HabitPlus/HabitPlus/Common/Network/WebService.swift
+++ SwiftUI/SwiftUITest/HabitPlus/HabitPlus/Common/Network/WebService.swift
@@ -1,24 +1,22 @@
 import Foundation
 
-// Vai ser um enum porque só vamos chamar os recursos de chamada dele
 enum WebService {
     
-    /*
-     {
-       "name": "string",
-       "email": "string",
-       "document": "string",
-       "phone": "string",
-       "gender": 0,
-       "birthday": "2024-06-28",
-       "password": "string"
-     }
-     */
-    
-    // Enum com todos os endpoints e baseUrl para fazer a chamada com a url certa
     enum Endpoint: String {
         case base = "https://habitplus-api.tiagoaguiar.co"
         case postUser = "/users"
+    }
+    
+    enum NetworkError {
+        case badRequest
+        case notFound
+        case unauthorized
+        case internalServerError
+    }
+    
+    enum Result {
+        case success(Data)
+        case failure(NetworkError, Data?)
     }
     
     enum HttpCases: String {
@@ -28,21 +26,23 @@ enum WebService {
         case delete = "DELETE"
     }
     
-    // Função para retornar a URLRequest com o path que escolhermos
     private static func completeUrl(path: Endpoint) -> URLRequest? {
         guard let url = URL(string: "\(Endpoint.base.rawValue)\(path.rawValue)") else { return nil }
-
+        
         return URLRequest(url: url)
     }
     
-    static func postUser(request: SignUpRequest) {
-                
-        guard let jsonData = try? JSONEncoder().encode(request) else { return }
+    // O tipo T é qualquer coisa que extenda do que passar no proximo
+    // Ou seja, agora essa função pode ser reutilizada para outras chamadas, no caso esse do post, de path tem o do post, e o de body ele tem o request que é do tipo SignUpRequest, que extende do Encodable e aí faz parte do tipo T que é genérico
+    private static func call<T: Encodable>(path: Endpoint, 
+                                           body: T,
+                                           completion: @escaping (Result) -> Void) {
         
-        guard var urlRequest = completeUrl(path: .postUser) else { return }
-        
+        guard var urlRequest = completeUrl(path: path) else { return }
+
+        guard let jsonData = try? JSONEncoder().encode(body) else { return }
+
         urlRequest.httpMethod = "POST"
-        
         urlRequest.setValue("application/json", forHTTPHeaderField: "accept")
         urlRequest.setValue("application/json", forHTTPHeaderField: "Content-Type")
         urlRequest.httpBody = jsonData
@@ -50,18 +50,34 @@ enum WebService {
         let task = URLSession.shared.dataTask(with: urlRequest) { data, response, error in
             guard let data = data, error == nil else {
                 print(error)
+                completion(.failure(.internalServerError, nil))
                 return
             }
             
-            print(String(data: data, encoding: .utf8))
-            print("response\n")
-            print(response)
-            
             if let r = response as? HTTPURLResponse {
-                print(r.statusCode)
+                switch r.statusCode {
+                case 200:
+                    completion(.success(data))
+                case 400:
+                    completion(.failure(.badRequest, data))
+                    break
+                default:
+                    break
+                }
             }
         }
-        
         task.resume()
+    }
+    
+    // Temos que receber de volta o erro ou dado caso tenha sido sucesso a requisição
+    static func postUser(request: SignUpRequest) {
+        call(path: .postUser, body: request) { result in
+            switch result {
+            case .failure(let error, let data):
+                break
+            case .success(let data):
+                break
+            }
+        }
     }
 }
--- SwiftUI/SwiftUITest/HabitPlus/HabitPlus/SignUp/ViewModel/SignUpViewModel.swift
+++ SwiftUI/SwiftUITest/HabitPlus/HabitPlus/SignUp/ViewModel/SignUpViewModel.swift
@@ -19,20 +19,17 @@ class SignUpViewModel: ObservableObject {
     func signUp() {
         self.uiState = .loading
         
-        // dd/MM/yyyy -> Date
         let formatter = DateFormatter()
         formatter.locale = Locale(identifier: "en_US_POSIX")
         formatter.dateFormat = "dd/MM/yyyy"
         
         let dateFormatted = formatter.date(from: birthday)
         
-        // Validar a data
         guard let dateFormatted = dateFormatted else {
             self.uiState = .error("Data inválida \(birthday)")
             return
         }
         
-        // Date -> yyyy-MM-dd
         formatter.dateFormat = "yyyy-MM-dd"
         let birthday = formatter.string(from: dateFormatted)
         
